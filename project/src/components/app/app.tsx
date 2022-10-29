f16ca931ed160d1bc7ca761e309604b371c12e3e
hinto <hinto@rambler.ru>
2022-10-29 16:49:36 +0700

    Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
+import { AppRoute } from '../../const';
+
 import Main from '../../pages/main/main';
+import SignIn from '../../pages/sign-in/sign-in';
+import MyList from '../../pages/my-list/my-list';
+import Film from '../../pages/film/film';
+import AddReview from '../../pages/add-review/add-review';
+import Player from '../../pages/player/player';
+import NotFound from '../../pages/not-found/not-found';
 
 type MainProps = {
   filmName: string;
@@ -7,5 +17,38 @@ type MainProps = {
 };
 
 export default function App(props: MainProps): JSX.Element {
-  return <Main filmName={props.filmName} genre={props.genre} data={props.data} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main filmName={props.filmName} genre={props.genre} data={props.data} />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyList />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<Film />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReview />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
