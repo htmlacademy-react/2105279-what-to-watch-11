0ca9641e123f3a5dd76c126f756acd43e87b27e3
hinto <hinto@rambler.ru>
2022-11-18 15:44:47 +0700

    Реализует возырат карточки в исходное состояние

--- project/src/components/video-player/video-player.tsx
+++ project/src/components/video-player/video-player.tsx
@@ -11,25 +11,13 @@ type VideoPlayerProps = {
 }
 
 export default function VideoPlayer({ isPlaying, src, previewImage, muted, width, height }: VideoPlayerProps): JSX.Element {
-  // const setIsLoading = useState(true)[1];
-
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
-    if (videoRef.current === null) {
-      return;
+    if (videoRef.current) {
+      isPlaying ? videoRef.current.play() : videoRef.current.src = src;
     }
-
-    if (isPlaying) {
-      videoRef.current.play();
-      return;
-    }
-
-    videoRef.current.pause();
-    videoRef.current.currentTime = 0;
-    // setIsLoading(false);
-
-  }, [isPlaying]);
+  }, [isPlaying, src]);
 
   return (
     <video
