7cedda3eaa93906982557d89f68836af2738bfca
hinto <hinto@rambler.ru>
2022-12-08 15:26:41 +0700

    Реализует отображение ошибки ответа сервера

--- project/src/components/error-message/error-message.tsx
+++ project/src/components/error-message/error-message.tsx
@@ -2,16 +2,15 @@
 import { useSelector } from 'react-redux';
 
 //Типы
-import { StateType } from '../../types/store';
+import { StoreType } from '../../store/index';
 
 //Компоненты
-// import './error-message.css';
+import './error-message.css';
 
 export default function ErrorMessage(): JSX.Element | null {
-  const error = useSelector((state: StateType) => state.error);
+  const error = useSelector((state: StoreType) => state.film.error);
 
   return (error)
-    // ? <div className='error-message'>{error}</div>
-    ? <div></div>
+    ? <div className='error-message'>{error}</div>
     : null;
 }
