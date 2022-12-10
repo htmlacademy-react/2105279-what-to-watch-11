b28e42583e504d56ed2c0196b1b2354905d09eb9
hinto <hinto@rambler.ru>
2022-12-10 16:59:23 +0700

    Добавляет селекторы для хранилища

---
+++ project/src/store/selectors.ts
@@ -0,0 +1,13 @@
+//Типы
+import { StoreType } from '../types/store';
+import { FilmData, CommentData } from '../types/film';
+import { AuthorizationStatus } from '../const';
+
+
+export const getFilms = (state: StoreType): FilmData[] => state.film.films;
+export const getFilm = (state: StoreType): FilmData => state.film.film as FilmData;
+export const getComments = (state: StoreType): CommentData[] => state.film.comments;
+export const getAuthorizationStatus = (state: StoreType): AuthorizationStatus => state.film.authorizationStatus;
+export const getCardCount = (state: StoreType): number => state.film.viewCardCount;
+export const getError = (state: StoreType): string | null => state.film.error;
+export const getLength = (state: StoreType): number => state.film.films.length;
