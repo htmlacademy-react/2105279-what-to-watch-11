2e5690aa65e14b3dcc859e5695206d68e74210be
hinto <hinto@rambler.ru>
2022-12-14 15:12:22 +0700

    реализует доступ myList

--- project/src/components/page-header/page-header.tsx
+++ project/src/components/page-header/page-header.tsx
@@ -1,11 +1,12 @@
 // Библиотеки
 import { SyntheticEvent } from 'react';
+import { Link } from 'react-router-dom';
 
 //Хуки
 import { useAppSelector, useAppDispatch } from '../../hooks';
 
 // Константы
-import { AuthorizationStatus } from '../../const';
+import { AuthorizationStatus, AppRoute } from '../../const';
 
 //Глобальное состояние
 import { logoutAction } from '../../store/api-actions';
@@ -24,9 +25,11 @@ export default function PageHeader({ favorite }: { favorite: number | null }) {
     ? (
       <ul className="user-block">
         <li className="user-block__item">
-          <div className="user-block__avatar">
-            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-          </div>
+          <Link to={AppRoute.MyList}>
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            </div>
+          </Link>
         </li>
         <li className="user-block__item">
           <a
